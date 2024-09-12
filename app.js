219c736ee8a676437a60682b758ec50ef734e6ba
henrique-Olivier <henrique.rosa@sptech.school>
2024-09-12 14:56:29 -0300

    whitepace finalizado

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const menu = document.querySelector('.menu')
+const overlay = document.querySelector('.overlay')
 const closeMenu = document.querySelector('.close-menu')
 
 function abrirMenu() {
     menu.classList.add('open')
+    overlay.classList.add('open')
 }
 
 function fecharMenu() {
     menu.classList.remove('open')
+    overlay.classList.remove('open')
 }
